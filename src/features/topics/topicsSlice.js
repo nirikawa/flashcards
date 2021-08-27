5e6a6347841f08485942fce95f23a1aee187f479
Nick <nirikawadev@gmail.com>
2021-08-27 10:30:37 -0700

    Implement steps 1-7

---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    topics: {}
+};
+
+const topicsSlice = createSlice({
+    name: 'topics',
+    initialState: initialState,
+    reducers: {
+        addTopic: (state, action) => {
+            const topic = {...action.payload, quizIds: []};
+            state.topics[action.payload.id] = topic;
+        }
+    }
+})
+
+export const selectTopics = (state) => state.topics;
+export const { addTopic } = topicsSlice.actions;
+export default topicsSlice.reducer;
