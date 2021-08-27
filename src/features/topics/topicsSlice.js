fd01b1e0e3523dd68df4b62a4eb92833976d0276
Nick <nirikawadev@gmail.com>
2021-08-27 16:55:28 -0700

    Hook the topic form up to slice action creators.

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -15,6 +15,6 @@ const topicsSlice = createSlice({
     }
 })
 
-export const selectTopics = (state) => state.topics;
+export const selectTopics = (state) => state.topics.topics;
 export const { addTopic } = topicsSlice.actions;
 export default topicsSlice.reducer;
